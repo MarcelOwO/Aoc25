2f5506687b68096eb00654c378947c32eff76aac
Marcel <Marcel-sigl@gmx.de>
2025-12-03 17:43:25 +0100

    fix solution with kpm algo

--- src/days/d_02.rs
+++ src/days/d_02.rs
@@ -24,11 +24,52 @@ pub(crate) fn is_valid_1(num: u64) -> bool {
         }
     }
 
-    return false;
+    false
+}
+
+pub(crate) fn create_lps(pattern: String) -> Vec<usize> {
+    let chars: Vec<char> = pattern.chars().collect();
+    let n = chars.len();
+
+    if n == 0 {
+        return Vec::new();
+    }
+
+    let mut lps = vec![0; n];
+
+    let mut len: usize = 0;
+    let mut i: usize = 1;
+
+    while i < n {
+        if chars[i] == chars[len] {
+            len += 1;
+            lps[i] = len;
+            i += 1;
+        } else if len != 0 {
+            len = lps[len - 1];
+        } else {
+            lps[i] = 0;
+            i += 1;
+        }
+    }
+    lps
 }
 
 pub(crate) fn is_valid_2(num: u64) -> bool {
     let str = num.to_string();
+    let str_len = str.len();
+    let lps = create_lps(str);
+    let overlap_len = lps[str_len - 1];
+    let pot_pat_len = str_len - overlap_len;
+    let has_overlap = overlap_len > 0;
+    let perfect_tiling = str_len.is_multiple_of(pot_pat_len);
+    has_overlap && perfect_tiling
+}
+/*
+pub(crate) fn is_valid_2(num: u64) -> bool {
+    let str = num.to_string();
+
+    let lps = create_lps(str.clone());
 
     'outer: for n in 0..str.len() / 2 {
         let pattern = String::from_str(str.split_at(n + 1).0).unwrap();
@@ -60,8 +101,9 @@ pub(crate) fn is_valid_2(num: u64) -> bool {
         return false;
     }
 
-    return true;
+    true
 }
+*/
 
 impl Solver for Day02Solver {
     fn solve1(&mut self, data: &str) {
@@ -100,11 +142,14 @@ impl Solver for Day02Solver {
             let end = end_str.parse::<u64>().unwrap();
 
             for n in start..end + 1 {
-                if !is_valid_2(n) {
+                if is_valid_2(n) {
                     counter += n;
                 }
             }
         }
+
+        //66500947346
+        //something like this
         println!("summ: {counter}");
     }
 }
